af12c0d8dc8a2ced56cc18fccedbd9bf756ec13b
Dmitriy Kyrov <kdg149@yandex.ru>
2022-08-25 22:39:04 +0300

    added case 4 in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,17 +8,21 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         List<String> list = new ArrayList<>();
-        list.add("one");
+        list.add("one"); //тестовый список
         list.add("two");
+        list.add("three two");
+        list.add("one three");
         list.add("three");
-
-        Iterator<String> listIter = list.iterator(); //создаем итератор
+        list.add("two three");
+        list.add("three");
+        list.add("three one");
 
         while (true) {
             System.out.println("""
                     1. Добавить
                     2. Показать
                     3. Удалить
+                    4. Найти
                     Выберите операцию или 'end'""");
             String input = scanner.nextLine();
             if ("end".equals(input)) {
@@ -45,31 +48,31 @@ public class Main {
                     System.out.println("Список покупок:");
                     shopList(list); //список покупок метод
                     System.out.println("Какую хотите удалить? Введите номер или название");
-
                     String del = scanner.nextLine(); //сканер
-                    if (tryParseInt(del)) {
-                        int num = Integer.parseInt(del) - 1; //переводим в число
-                        String removedStrNum = list.remove(num); //удаляем
-                        removedStr(removedStrNum); //покупка удалена метод
-                    } else if (!tryParseInt(del)){
-                        String removedStrName = list.remove(list.indexOf(del));
-                        removedStr(removedStrName); //покупка удалена метод
-                    }
-
-//                    while (listIter.hasNext()) { //до тех пор, пока в списке есть элементы
-//                        String removedStrName = listIter.next(); //получаем следующий элемент
-//                        if (removedStrName.equals(del)) { //прверка на совпадение
-//                            listIter.remove(); //удаляем
-//                            removedStr(removedStrName); //покупка удалена метод
-//                        }
-//                    }
-
+                    deleteListItem(del, list); //удаление покупки метод
                     shopList(list); //список покупок метод
+                    separator(); //разделитель метод
+                    break;
+                case 4:
+                    System.out.println("Введите текст для поиска:");
+                    String item = scanner.nextLine(); //сканер
+                    System.out.println("Найдено:");
+                    searchListItem(item, list); //поиск покупки метод
                     separator(); //разделитель метод
                     break;
                 default:
                     System.out.println("Такой операции нет");
                     separator(); //разделитель метод
+            }
+        }
+    }
+
+    static void searchListItem(String item, List<String> list) {
+        String queryLower = item.toLowerCase();
+        for (int i = 0; i < list.size(); i++) {
+            String itemLower = list.get(i).toLowerCase();
+            if (itemLower.contains(queryLower)) {
+                System.out.println((i + 1) + ". " + list.get(i));
             }
         }
     }
@@ -83,18 +86,29 @@ public class Main {
         }
     }
 
-    public static void separator() {
+    static void deleteListItem(String del, List<String> list) {
+        if (tryParseInt(del)) {
+            int num = Integer.parseInt(del) - 1; //переводим в число
+            String removedStrNum = list.remove(num); //удаляем
+            removedStr(removedStrNum); //покупка удалена метод
+        } else if (!tryParseInt(del)) {
+            String removedStrName = list.remove(list.indexOf(del));
+            removedStr(removedStrName); //покупка удалена метод
+        }
+    }
+
+    static void separator() {
         System.out.println("^^^^^^^^^^^^^^^^^^^^^^^^^^");
     }
 
-    public static void shopList(List<String> list) {
+    static void shopList(List<String> list) {
         for (int i = 0; i < list.size(); i++) {
             System.out.println((i + 1)
                     + ". " + list.get(i));
         }
     }
 
-    public static void removedStr(String removedStr) {
+    static void removedStr(String removedStr) {
         System.out.println("Покупка '"
                 + removedStr
                 + "' удалена, список покупок:");
